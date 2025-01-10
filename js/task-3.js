b7e50c79f82284c7da34295d3f73678cea0efbe9
JuliiaTovchko <166376280+JuliiaTovchko@users.noreply.github.com>
2025-01-10 10:33:16 +0200

    add index.html file

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,9 @@
 function getElementWidth(content, padding, border) {
-   
-const contentWidth = parseFloat(content);
-const paddingWidth = parseFloat(padding);
-const borderWidth = parseFloat(border);
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding) * 2; 
+    const borderWidth = parseFloat(border) * 2;  
     
-return contentWidth + paddingWidth + borderWidth;
+    return contentWidth + paddingWidth + borderWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
